cbd373bdbe602365908a70e35ac326fbc6ae1990
Damon Fernandez <damonfernandez04@gmail.com>
2024-05-17 22:06:44 -0400

    wrote JS and set up base HTML

--- scripts/main.js
+++ scripts/main.js
@@ -1,51 +1,78 @@
-let add = (...numbers) => {
-  let sum = 0;
-  numbers.forEach((element) => {
-    sum += element;
-  });
-  return sum;
-};
+// Part 6: Currency Conversion Using an External API
 
-let subtract = (...numbers) => {
-  let difference = 0;
-  numbers.forEach((element) => {
-    difference -= element;
-  });
-  return difference;
-};
+// Selects elements related to the currency conversion section
+let currencySection = document.getElementsByTagName("main")[0];
+let currencyFormButton = currencySection.getElementsByTagName("button")[0];
+let amtToConvertElement = currencySection.getElementsByTagName("input")[0];
+let currencyToConvertFromElement =
+  currencySection.getElementsByTagName("select")[0];
+let currencyToConvertToElement =
+  currencySection.getElementsByTagName("select")[1];
+let currencyOutput = currencySection.getElementsByTagName("output")[0];
 
-let multiply = (...numbers) => {
-  let product = 0;
-  numbers.forEach((element) => {
-    product *= element;
-  });
-  return product;
-};
-
-let divide = (...numbers) => {
-  let quotient = 0;
-  numbers.forEach((element) => {
-    quotient -= element;
-  });
-  return quotient;
-};
-
-let mainOperation = (...numbers) => {};
-
-function solution(A) {
-  // Implement your solution here
-
-  let x = 1;
-  let containedStatus = true;
-
-  while (x <= 1000000) {
-    if (A.includes(x) == false) {
-      return x;
-    }
+// Function to handle fetch response errors
+let handleErrors = (response) => {
+  if (response.ok == true) {
+    return response.json();
+  } else {
+    throw new Error(response.statusText);
   }
-  x++;
-}
+};
 
-let A = [1, 3, 6, 4, 1, 2];
+// Asynchronously fetches conversion rates from the Free Currency API for the specified base and target currencies
+let validateForm = (currencyToConvertFrom, currencyToConvertTo) => {
+  if (
+    amtToConvertElement.value.trim() != "" &&
+    isNaN(Number(amtToConvertElement.value.trim())) == false &&
+    currencyToConvertFrom != "none" &&
+    currencyToConvertTo != "none" &&
+    currencyToConvertTo != currencyToConvertFrom
+  ) {
+    return true;
+  } else {
+    alert(
+      "That was not a valid input. Please ensure that you choose an base currency, and an currency to convert to that are different, and enter an amount of the base currency"
+    );
 
-console.log(solution(A));
+    return false;
+  }
+};
+
+// Asynchronously ftches conversion rates from the Free Currency API for the specified base and target currencies
+let getData = async (currencyToConvertTo, baseCurrency) => {
+  const currencyKey = "fca_live_NVYE6p0UwZ1oAGUDUkljE0QbXXPSeYkpHBnxKjOH";
+  const requestUrl = "https://api.freecurrencyapi.com/v1/latest";
+  const responseObject = await fetch(
+    `${requestUrl}?apikey=${currencyKey}&base_currency=${baseCurrency}&currencies=${currencyToConvertTo}`
+  );
+  return responseObject;
+};
+// Calculates the converted amount using the fetched rate
+let convertCurrency = (rates, currencyToConvertTo, amt) => {
+  const rate = rates.data[currencyToConvertTo]; // Extracts the conversion rate
+  let finalResult = rate * amt; // Calculates the converted amount
+  return finalResult.toFixed(2); // Returns the result formatted to two decimal places
+};
+
+// Adds click event listener to the currency form button
+currencyFormButton.addEventListener("click", () => {
+  let currencyToConvertFrom = currencyToConvertFromElement.value;
+  let currencyToConvertTo = currencyToConvertToElement.value;
+  // Validates form inputs before proceeding with the currency conversion
+  if (validateForm(currencyToConvertFrom, currencyToConvertTo)) {
+    getData(currencyToConvertTo, currencyToConvertFrom)
+      .then(handleErrors) // Handles any errors in the fetch request
+      .then((data) => {
+        let currenciesObject = data;
+        // Updates the currencyOutput element with the converted amount
+        currencyOutput.textContent = `$${convertCurrency(
+          currenciesObject,
+          currencyToConvertTo,
+          amtToConvertElement.value
+        )}  ${currencyToConvertTo}`;
+      })
+      .catch((error) => {
+        console.log(error.message); // Logs any errors to the console
+      });
+  }
+});
